78b340cb32a052b0af8f9e41a94c7d4c65b9fbcc
gangadharsingh <gangadharsingh056@msitprogram.net>
2018-09-27 17:26:42 +0530

    "check_style_score -> 24 of 24 passed. style score: 1/1 "

---
+++ assignment/m2/CodeCamp_Percolation/Solution.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+/**
+ * Class for percolation.
+ * @author: gangadharsingh.
+ */
+/**
+ * Class for solution.
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+    }
+    /**
+     * creating main method.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        /**
+         * checking if percolates or not
+         */
+        Scanner scan = new Scanner(System.in);
+        int n = Integer.parseInt(scan.nextLine());
+        Percolation percolate = new Percolation(n);
+        while (scan.hasNextLine()) {
+            String[] input = scan.nextLine().split(" ");
+            percolate.open(Integer.parseInt(input[0]) - 1,
+                Integer.parseInt(input[1]) - 1);
+        }
+        System.out.println(percolate.percolates());
+    }
+}
