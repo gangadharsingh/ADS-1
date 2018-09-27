40769c9a83a491149df9160d5dc1d6dcd38ef2d6
gangadharsingh <gangadharsingh056@msitprogram.net>
2018-09-27 17:21:49 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- assignment/m4/4.1 Balanced Paranthesis/Solution.java
+++ assignment/m4/4.1 Balanced Paranthesis/Solution.java
@@ -1,36 +1,21 @@
 import java.util.Scanner;
-class Linkedlist {
-	private Node firstelement = null;
-	private class Node {
-		char item;
-		Node nextAddress;
-	}
-	public char topelement() {
-		return firstelement.item;
-	}
-
-	public boolean isEmpty() {
-		return firstelement == null;
-	}
-	public void push(char item) {
-		Node oldfirst = firstelement;
-		firstelement = new Node();
-		firstelement.item = item;
-		firstelement.nextAddress = oldfirst;
-	}
-	public char pop() {
-		char item = firstelement.item;
-		firstelement = firstelement.nextAddress;
-		return item;
-	}
-}
+/**
+ * Class for solution.
+ * @author: gangadharsingh
+ */
 public class Solution {
-	public static void main(String[] args) {
+	/**
+	 * { main class }.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int size = Integer.parseInt(sc.nextLine());
+		Balanceparenthesis balance = new Balanceparenthesis();
 		while (size > 0) {
 			String input = sc.next();
-			if(checkParanthesis(input)){
+			if(balance.checkParanthesis(input)){
 				System.out.println("YES");
 			} else {
 				System.out.println("NO");
@@ -38,25 +23,4 @@ public class Solution {
 			size--;
 		}
 	}
-	public static boolean checkParanthesis(String str) {
-		Linkedlist obj = new Linkedlist();
-		int strLength = str.length();
-		for (int i = 0; i < strLength; i++) {
-			char character = str.charAt(i);
-			if (character == '(' || character == '{' || character == '[') {
-				obj.push(character);
-			}
-			else {
-				if(obj.isEmpty()) return false;
-				if (character == ')' && obj.topelement() == '(') {
-				obj.pop();
-			} else if (character == '}' && obj.topelement() == '{') {
-				obj.pop();
-			} else if (character == ']' && obj.topelement() == '[') {
-				obj.pop();
-			} else return false;
-		}
-	}
-	return obj.isEmpty();
-}
 }
