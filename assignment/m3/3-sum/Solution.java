0dcaa2f06c101b2a11c64f7b0f69e06cf75c271d
gangadharsingh <gangadharsingh056@msitprogram.net>
2018-09-27 20:44:04 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- assignment/m3/3-sum/Solution.java
+++ assignment/m3/3-sum/Solution.java
@@ -5,70 +5,67 @@ import java.util.Arrays;
  * @author: gangadharsingh.
  */
 public class Solution {
-	/**
-	*constructor.
-	*/
-	Solution() { }
-	/**
-	 * main function.
-	 *
-	 * @param      args  The arguments.
-	 */
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int num = scan.nextInt();
-		int[] arr1 = new int[num];
-		for (int i = 0;  i < num; i++) {
-			arr1[i] = scan.nextInt();
-		}
-		Arrays.sort(arr1);
-		Threesum three = new Threesum(arr1);
-		System.out.println(three.tSum());
-	}
+    /**
+    *constructor.
+    */
+    private Solution() { }
+    /**
+     * main function.
+     *
+     * @param      args  The arguments.
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        int[] arr1 = new int[num];
+        for (int i = 0;  i < num; i++) {
+            arr1[i] = scan.nextInt();
+        }
+        Arrays.sort(arr1);
+        Threesum three = new Threesum(arr1);
+        System.out.println(three.tSum());
+    }
 }
 /**
  * Class for threesum.
  */
 class Threesum {
-	/**
-	 * { var_description }.
-	 */
-	private int[] array;
-	/**
-	 * Constructs the object.
-	 *
-	 * @param      arrthreesum  The arrthreesum
-	 */
-	Threesum(int[] arrthreesum) {
-		array = arrthreesum;
-	}
-	/**
-	 * { function_description }.
-	 *
-	 * @return     { description_of_the_return_value }.
-	 */
-	int tSum() {
-	
-			int low, last;
-			int count = 0;
-			for (int s: array) {
-			}
-			for (int i = 0; i < array.length-2; i++) {
-				low = i+1;
-				last = array.length-1;
-				while (low < last) {
-						int total = array[low] +array[last];
-						if (array[low] +array[last]+ array[i] < 0) {
-							low++;
-						} else if (array[low] +array[last]+ array[i] > 0) {
-							last--;
-						} else {
-							count++;
-							low++;
-							last--;
-						}
-				}
-			}
-			return count;
+    /**
+     * { var_description }.
+     */
+    private int[] array;
+    /**
+     * Constructs the object.
+     *
+     * @param      arrthreesum  The arrthreesum
+     */
+    Threesum(final int[] arrthreesum) {
+        array = arrthreesum;
+    }
+    /**
+     * { function_description }.
+     *
+     * @return     { description_of_the_return_value }.
+     */
+    int tSum() {
+        int low, last;
+        int count = 0;
+        for (int i = 0; i < array.length - 2; i++) {
+            low = i + 1;
+            last = array.length - 1;
+            while (low < last) {
+                int total = array[low] + array[last];
+                if (array[low] + array[last] + array[i] < 0) {
+                    low++;
+                } else if (array[low] + array[last] + array[i] > 0) {
+                    last--;
+                } else {
+                    count++;
+                    low++;
+                    last--;
+                }
+            }
         }
+        return count;
+    }
 }
