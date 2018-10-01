cfa6d1f1896be930f5f59a6a3f39af6926e65006
gangadharsingh <gangadharsingh056@msitprogram.net>
2018-10-01 18:21:09 +0530

    "check_style_score -> 2 of 2 passed. style score: 1/1 "

--- assignment/m7/7.1 Steque/Solution.java
+++ assignment/m7/7.1 Steque/Solution.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 /**
  * Class for solution.
  */
-public class Solution {
+public final class Solution {
     /**
      * Constructs the object.
      */
@@ -13,36 +13,35 @@ public class Solution {
      *
      * @param      args  The arguments
      */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
         int num1 = Integer.parseInt(scan.nextLine());
         while (num1 >= 0) {
             Steque st = new Steque();
-            while(scan.hasNextLine()) {
+            while (scan.hasNextLine()) {
                 String line =  scan.nextLine();
                 String[] command = line.split(" ");
-                    switch (command[0]) {
-                    case "push":
-                        st.push(command[1]);
-                        System.out.println(st.print());
-                        break;
-                    case "enqueue":
-                        st.enqueue(command[1]);
-                        System.out.println(st.print());
-                        break;
-                    case "pop":
-                        st.pop();
-                            System.out.println(st.print());
-                        break;
-                    default:
-                        break;
-                    }
-                    if(line.equals("")){
-                        System.out.println();
-                        break;
-                    }
+                switch (command[0]) {
+                case "push":
+                    st.push(command[1]);
+                    System.out.println(st.print());
+                    break;
+                case "enqueue":
+                    st.enqueue(command[1]);
+                    System.out.println(st.print());
+                    break;
+                case "pop":
+                    st.pop();
+                    System.out.println(st.print());
+                    break;
+                default:
+                    break;
                 }
-            
+                if (line.equals("")) {
+                    System.out.println();
+                    break;
+                }
+            }
             num1--;
         }
     }
