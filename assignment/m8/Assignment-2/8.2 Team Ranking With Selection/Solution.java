a531e5d764add91d2a604bf35d65cbb1b6b47f01
gangadharsingh <gangadharsingh056@msitprogram.net>
2018-10-03 00:19:51 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- assignment/m8/Assignment-2/8.2 Team Ranking With Selection/Solution.java
+++ assignment/m8/Assignment-2/8.2 Team Ranking With Selection/Solution.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
  * Class for solution.
  */
 class Solution {
-	/**
+    /**
      * Constructs the object.
      */
     private Solution() { }
@@ -11,30 +11,30 @@ class Solution {
      * magic number.
      */
     public static final int NUM = 10;
-	/**
-	 * {main function}.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		Team[] tm = new Team[NUM];
-		int size = 0;
-        while (sc.hasNextLine()){
-	        String input = sc.nextLine();
-			if (input.equals("")) {
-				break;
-			}
-	        String[] tokens =  input.split(",");
-			tm[size++] = new Team(tokens);
-    	}
-    	SelectionSort select = new SelectionSort(tm, size);
-    	tm = select.sort();
-    	String str = "";
-    	for (int i  = 0; i < size-1; i ++) {
-    		str += tm[i].toString() + ",";
-    	}
-    	str += tm[size-1].toString();
-    	System.out.println(str);
-	}
+    /**
+     * {main function}.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Team[] tm = new Team[NUM];
+        int size = 0;
+        while (sc.hasNextLine()) {
+            String input = sc.nextLine();
+            if (input.equals("")) {
+                break;
+            }
+            String[] tokens =  input.split(",");
+            tm[size++] = new Team(tokens);
+        }
+        SelectionSort select = new SelectionSort(tm, size);
+        tm = select.sort();
+        String str = "";
+        for (int i  = 0; i < size - 1; i++) {
+            str += tm[i].toString() + ",";
+        }
+        str += tm[size - 1].toString();
+        System.out.println(str);
+    }
 }
