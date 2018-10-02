82f49fa7d9701d3e4274636e68ead38f04c06469
gangadharsingh <gangadharsingh056@msitprogram.net>
2018-10-03 00:21:15 +0530

    "check_style_score -> 6 of 6 passed. style score: 1/1 "

--- assignment/m8/Assignment-2/8.2 Team Ranking With Selection/Solution.java
+++ assignment/m8/Assignment-2/8.2 Team Ranking With Selection/Solution.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 /**
  * Class for solution.
  */
-class Solution {
+public final class Solution {
     /**
      * Constructs the object.
      */
@@ -16,7 +16,7 @@ class Solution {
      *
      * @param      args  The arguments
      */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
         Team[] tm = new Team[NUM];
         int size = 0;
