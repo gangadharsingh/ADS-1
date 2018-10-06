4647b974929ad01d884e21269bfe8fbaff90c3f8
gangadharsingh <gangadharsingh056@msitprogram.net>
2018-10-06 13:33:21 +0530

    "ads1_m12_a1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignment/m12/Assignment-1/Mergesort.java
+++ assignment/m12/Assignment-1/Mergesort.java
@@ -9,9 +9,6 @@ public class Mergesort {
         }
         int i = lo, j = mid+1;
         for (int k = lo; k <= hi; k++) {
-            if (aux[i] == null || aux[j] == null) {
-                break;
-            }
             if      (i > mid)              a[k] = aux[j++];
             else if (j > hi)               a[k] = aux[i++];
             else if (less(aux[j], aux[i])) a[k] = aux[j++];
