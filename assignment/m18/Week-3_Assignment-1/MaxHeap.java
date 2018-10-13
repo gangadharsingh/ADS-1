6fd6673d0ad2a4ad1f236b910278c8280c847712
gangadharsingh <gangadharsingh056@msitprogram.net>
2018-10-13 13:09:07 +0530

    "ads1_week3_exam: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignment/m18/Week-3_Assignment-1/MaxHeap.java
+++ assignment/m18/Week-3_Assignment-1/MaxHeap.java
@@ -1,234 +1,117 @@
-import java.util.Comparator;
-// /**
-//  * Class for maximum heap.
-//  *
-//  * @param      <Key>  The key
-//  */
-// class MaxHeap<Key> {
-// 	/**
-// 	 * { var_description }
-// 	 */
-// 	public Key[] hp;
-// 	/**
-// 	 * { var_description }
-// 	 */
-// 	public int size;
-// 	/**
-// 	 * { var_description }
-// 	 */
-// 	public Comparator<Key> comparator;
-// 	/**
-// 	 * Constructs the object.
-// 	 *
-// 	 * @param      sz    The size
-// 	 */
-// 	public MaxHeap(int sz) {
-// 		hp = (Key[]) new Object[sz + 1];
-// 		size = 0;
-// 	}
-// 	/**
-// 	 * Constructs the object.
-// 	 */
-// 	public MaxHeap() {
-// 		this(1);
-// 	}
-// 	/**
-// 	 * { function_description }
-// 	 *
-// 	 * @return     { description_of_the_return_value }
-// 	 */
-// 	public int getsize() {
-// 		return size;
-// 	}
-// 	/**
-// 	 * { function_description }
-// 	 *
-// 	 * @param      k     { parameter_description }
-// 	 */
-// 	public void insertMax(Key k) {
-// 		if (size == hp.length - 1) {
-// 			resize(2 * hp.length);
-// 		}
-// 		hp[++size] = k;
-// 		swimMax(size);
-// 	}
-// 	/**
-// 	 * { function_description }
-// 	 *
-// 	 * @param      ind   The ind
-// 	 */
-// 	public void swimMax(int ind) {
-// 		while (ind > 1 && minHP(ind / 2, ind)) { //ind is child and ind/2 is parent.
-// 			swap(ind, ind / 2); //exchange parent with child when parent is less than child.
-// 			ind = ind / 2;
-// 		}
-// 	}
-// 	/**
-// 	 * { function_description }
-// 	 *
-// 	 * @param      i     { parameter_description }
-// 	 * @param      j     { parameter_description }
-// 	 *
-// 	 * @return     { description_of_the_return_value }
-// 	 */
-// 	public boolean minHP(int i, int j) {
-// 		if (comparator == null) {
-// 			return  ((Comparable<Key>) hp[i]).compareTo(hp[j]) < 0;
-// 		} else {
-// 			return comparator.compare(hp[i], hp[j]) < 0;
-// 		}
-// 	}
-// 	/**
-// 	 * Gets the maximum.
-// 	 *
-// 	 * @return     The maximum.
-// 	 */
-// 	public Key getMax() {
-// 		return hp[1];
-// 	}
-// 	/**
-// 	 * { function_description }
-// 	 *
-// 	 * @return     { description_of_the_return_value }
-// 	 */
-// 	public Key delMax() {
-// 		Key max = hp[1];
-// 		swap(1, size--);
-// 		sinkMax(1);
-// 		hp[size + 1] = null;
-// 		if ((size > 0) && (size == (hp.length - 1) / 4)) {
-// 			resize(hp.length / 2);
-// 		}
-// 		return max;
-// 	}
-// 	/**
-// 	 * { function_description }
-// 	 *
-// 	 * @param      a     { parameter_description }
-// 	 */
-// 	public void sinkMax(int a) {
-// 		while (2 * a <= size) {
-// 			int j = 2 * a;
-// 			if (j < size && minHP(j, j + 1)) {
-// 				j++;
-// 			}
-// 			swap(a, j);
-// 			a = j;
-// 		}
-// 	}
-// 	public void resize(int newsize) {
-// 		Key[] temp = (Key[]) new Object[newsize];
-// 		for (int i = 1; i <= size; i++) {
-// 			temp[i] = hp[i];
-// 		}
-// 		hp = temp;
-// 	}
-// 	public void swap(int i, int j) {
-// 		Key temp = hp[i];
-// 		hp[i] = hp[j];
-// 		hp[j] = temp;
-// 	}
-// 	public void show() {
-// 		for (int i = 1; i < size + 1; i++) {
-// 			System.out.print(hp[i] + " ");
-// 		}
-// 		System.out.println();
-// 	}
-// }
+/**
+ * Class for maximum heap.
+ */
 class MaxHeap {
-    /**
-     * array.
-     */
-    private Stockdata[] array;
-    /**
-     * size.
-     */
-    private int size;
-    /**
-     * Constructs the object.
-     *
-     * @param      a     { parameter_description }
-     * @param      n     { parameter_description }
-     */
-    MaxHeap(final Stockdata[] a, final int n) {
-        array = a;
-        size = n;
-    }
-    /**
-     * sorts.
-     * Best case: O(N)
-     *  worst case: O(N)
-     *  Average case: O(N)
-     * @return     { description_of_the_return_value }
-     */
-    Stockdata[] sort() {
-        for (int i = (size / 2) - 1; i >= 0; i--) {
-            heapify(size, i);
-        }
-        for (int i = size - 1; i >= 0; i--) {
-            swap(0, i);
-            heapify(i, 0);
-        }
-        return array;
-    }
-    /**
-     * swaps.
-     *  Best case: O(1)
-     *  worst case: O(1)
-     *  Average case: O(1)
-     * @param      i     { parameter_description }
-     * @param      j     { parameter_description }
-     */
-    void swap(final int i, final int j) {
+	/**
+	 * array.
+	 */
+	private Stockdata[] array;
+	/**
+	 * size.
+	 */
+	private int size;
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      a     { parameter_description }
+	 * @param      n     { parameter_description }
+	 */
+	MaxHeap(final Stockdata[] a, final int n) {
+		array = a;
+		size = n;
+	}
+	/**
+	 * sorts.
+	 * Best case: O(N)
+	 *  worst case: O(N)
+	 *  Average case: O(N)
+	 * @return     { description_of_the_return_value }
+	 */
+	Stockdata[] sort() {
+		for (int i = (size / 2) - 1; i >= 0; i--) {
+			heapifyUp(i);
+		}
+		for (int i = size - 1; i >= 0; i--) {
+			swap(0, i);
+			heapifyUp(0);
+		}
+		return array;
+	}
+	/**
+	 * swaps.
+	 *  Best case: O(1)
+	 *  worst case: O(1)
+	 *  Average case: O(1)
+	 * @param      i     { parameter_description }
+	 * @param      j     { parameter_description }
+	 */
+	void swap(final int i, final int j) {
+		Stockdata temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
+	/**
+	 * heapifies.
+	 *  Best case: O(logN)
+	 *  worst case: O(logN)
+	 *  Average case: O(logN)
+	 * @param      n     { parameter_description }
+	 * @param      i     { parameter_description }
+	 */
+	// void heapify(final int n, final int i) {
+	// 	int min = i;
+	// 	int l = 2 * i + 1;
+	// 	int r = 2 * i + 2;
+	// 	if (l < n && array[l].compareTo(array[min]) < 0) {
+	// 		min = l;
+	// 	}
+	// 	if (r < n && array[r].compareTo(array[min]) < 0) {
+	// 		min = r;
+	// 	}
+	// 	if (min != i) {
+	// 		swap(min, i);
+	// 		heapify(min, n);
+	// 	}
+	// }
+	public void heapifyUp(int i) {
         Stockdata temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
-    /**
-     * heapifies.
-     *  Best case: O(logN)
-     *  worst case: O(logN)
-     *  Average case: O(logN)
-     * @param      n     { parameter_description }
-     * @param      i     { parameter_description }
-     */
-    void heapify(final int n, final int i) {
-        int min = i;
-        int l = 2 * i + 1;
-        int r = 2 * i + 2;
-        if (l < n && array[l].compareTo(array[min]) > 0) {
-            min = l;
+        while(i>0 && temp.compareTo(array[(i-1)/2]) > 0){
+            array[i] = array[(i-1)/2];
+            i = (i-1)/2;
         }
-        if (r < n && array[r].compareTo(array[min]) > 0) {
-            min = r;
-        }
-        if (min != i) {
-            swap(i, min);
-            heapify(n, min);
-        }
+        array[i] = temp;
     }
-    /**
-     * gets item.
-     *
-     * Best case: O(1)
-     *  worst case: O(1)
-     *  Average case: O(1)
-     * @param      index  The index
-     *
-     * @return     { description_of_the_return_value }
-     */
-    Stockdata getitem(final int index) {
-        return array[index];
-    }
-    /**
-     * get size.
-     *
-     * Best case: O(1)
-     *  worst case: O(1)
-     *  Average case: O(1)
-     * @return     { description_of_the_return_value }
-     */
-    int getsize() {
-        return size;
-    }
+	/**
+	 * gets item.
+	 *
+	 * Best case: O(1)
+	 *  worst case: O(1)
+	 *  Average case: O(1)
+	 * @param      index  The index
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
+	Stockdata getitem(final int index) {
+		return array[0];
+	}
+	/**
+	 * get size.
+	 *
+	 * Best case: O(1)
+	 *  worst case: O(1)
+	 *  Average case: O(1)
+	 * @return     { description_of_the_return_value }
+	 */
+	int getsize() {
+		return size;
+	}
+	/**
+	 * { function_description }
+	 */
+	void print() {
+		for (int i = 0; i < array.length - 1; i++) {
+			System.out.print(array[i]+"--");
+		}
+		System.out.print(array[array.length - 1]);
+	}
 }
--- assignment/m18/Week-3_Assignment-1/Solution.java
+++ assignment/m18/Week-3_Assignment-1/Solution.java
@@ -1,16 +1,29 @@
 import java.util.Scanner;
+/**
+ * Class for solution.
+ */
 class Solution {
+	/**
+	 * Constructs the object.
+	 */
 	Solution() { }
+	/**
+	 * { function_description }
+	 *
+	 * @param      args  The arguments
+	 */
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int numStock = Integer.parseInt(scan.nextLine());
-		int run = numStock*6;
-		MinHeap minHp; /*= new MinHeap<Stockdata>();*/
-		MaxHeap maxHp;/* = new MaxHeap<Stockdata>();*/
+		int run = numStock*(2+2+2);
+		MinHeap minHp;
+		MaxHeap maxHp;
+		Stockdata[] stdata = new Stockdata[numStock];
+		Stockdata[] freqBest = new Stockdata[(2+2+1)*numStock];;
+		Stockdata[] freqWorst = new Stockdata[(2+2+1)*numStock];
+		SymbolTable<String, Integer> bestStock = new SymbolTable<String, Integer>();
+		SymbolTable<String, Integer> worstStock = new SymbolTable<String, Integer>();
 		while(run > 0) {
-			Stockdata[] stdata = new Stockdata[numStock];
-			Stockdata[] freqBest = new Stockdata[5];
-			Stockdata[] freqWorst = new Stockdata[5];
 			for (int i = 0; i < numStock; i++) {
 				String[] testcase = scan.nextLine().split(",");
 				stdata[i] = new Stockdata(testcase[0], Float.parseFloat(testcase[1]));
@@ -18,48 +31,148 @@ class Solution {
 				// System.out.println(stdata[i]);
 			}
 			minHp = new MinHeap(stdata, numStock);
-			minHp.sort();
 			maxHp = new MaxHeap(stdata, numStock);
+			minHp.sort();
 			maxHp.sort();
-			for (int i = 1; i <= 5; i++) {
-				freqWorst[i-1] = minHp.getitem(i);
-				freqBest[i-1] = maxHp.getitem(i);
+			bestStock = new SymbolTable<String, Integer>();
+			// minHp.print();
+			for (int i = 0; i < 5; i++) {
+				freqWorst[i] = minHp.getitem(i);
+				freqBest[i] = maxHp.getitem(i);
 			}
-			for (int i = 1; i <= 5; i++) {
-				System.out.println(freqBest[i]);
-				System.out.println("worst");
-				System.out.println(freqWorst[i]);
-			}
+			// maxHp.print();
+			// System.out.println("--------------------------------------");
+	  		//bestStock.put(freqBest[i], i);
 			run--;
 		}
+		int[] bestcount = new int[freqBest.length];
+		int[] worstcount = new int[freqWorst.length];
+		int bcount = 0;
+		int wcount = 0;
+		String[] beststock = new String[freqBest.length];
+		String[] worststock = new String[freqWorst.length];
+		for (int i = 0; i < freqBest.length; i++) {
+			for (int j = 1; j < freqBest.length-1; j++) {
+				if (freqBest[i].getname().equals(freqBest[j].getname())) {
+					beststock[i] = freqBest[i].getname();
+					bestcount[i] = bcount++;
+				} if (freqWorst[i].getname().equals(freqWorst[j].getname())) {
+					worststock[i] = freqWorst[i].getname();
+					worstcount[i] = wcount++;
+				}
+			}
+		}
+		for (int i = 0; i < freqBest.length; i++) {
+			for (int j = 0; j < (2+2+1); j++) {
+				System.out.println(freqBest[i].getname()+" "+freqBest[i].getperChange());
+			}
+			System.out.println();
+			for (int j = 0; j < (2+2+1); j++) {
+				System.out.println(freqWorst[i].getname()+" "+freqWorst[i].getperChange());
+			}
+			System.out.println();
+		}
+		// for (int i = 0; i < freqBest.length; i++) {
+		// 	bestStock.put(beststock[i], bestcount[i]);
+		// 	worstStock.put(worststock[i], worstcount[i]);
+		// }
+		int query = Integer.parseInt(scan.nextLine());
+		stdata = new Stockdata[numStock];
 	}
 }
 
+/**
+ * Class for stockdata.
+ */
 class Stockdata {
+	/**
+	 * { var_description }
+	 */
 	String name;
+	/**
+	 * { var_description }
+	 */
 	Float perChange;
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      nm       { parameter_description }
+	 * @param      pchange  The pchange
+	 */
 	Stockdata(String nm, Float pchange) {
 		name = nm;
 		perChange = pchange;
 	}
+	/**
+	 * { function_description }
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
 	public String getname(){
 	  return name;
 	}
+	/**
+	 * { function_description }
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
 	public Float getperChange(){
 	  return perChange;
 	}
+	/**
+	 * Returns a string representation of the object.
+	 *
+	 * @return     String representation of the object.
+	 */
 	public String toString() {
 		return name + " " + perChange;
 	}
+	/**
+	 * { function_description }
+	 *
+	 * @param      that  The that
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
 	public int compareTo(final Stockdata that) {
-        if (this.getperChange() > that.getperChange()) {
+        if (this.getperChange() < that.getperChange()) {
             return 1;
-        } else if (this.getperChange() < that.getperChange()) {
+        } else if (this.getperChange() > that.getperChange()) {
             return -1;
         }
         return 0;
     }
 }
+/**
+ * Class for querydata.
+ */
 class Querydata {
-	Querydata() { }
+	/**
+	 * { var_description }
+	 */
+	SymbolTable<String, Integer> stock;
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      st    { parameter_description }
+	 */
+	Querydata(SymbolTable<String, Integer> st) {
+		stock = st;
+	}
+	/**
+	 * { function_description }
+	 *
+	 * @param      s     { parameter_description }
+	 * @param      name  The name
+	 */
+	void get(String s, String name) {
+		String str = s.substring(0,2);
+		if (str.equals("max")) {
+			for (int i = 0; i < stock.size();i++ ) {
+				
+			}
+		} else if (str.equals("min")) {
+			
+		}
+	}
 }
