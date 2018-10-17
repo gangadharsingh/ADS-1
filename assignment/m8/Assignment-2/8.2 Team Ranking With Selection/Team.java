dc2f64a45165153bd79d4b9084b5722758bfe75a
gangadharsingh <gangadharsingh056@msitprogram.net>
2018-10-17 12:06:56 +0530

    "ads1_m8_a2: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- assignment/m8/Assignment-2/8.2 Team Ranking With Selection/Team.java
+++ assignment/m8/Assignment-2/8.2 Team Ranking With Selection/Team.java
@@ -1,42 +1,107 @@
-class Team {
-	String teamname;
-	int wins;
-	int loses;
-	int draw;
-	Team() { }
-	Team(String[] inp) {
+/**.
+ * Class for team.
+ */
+public class Team {
+	/**.
+	 * { teamname .
+	 */
+	private String teamname;
+	/**.
+	 * { var_description .
+	 */
+	private int wins;
+	/**.
+	 * { var_description .
+	 */
+	private int loses;
+	/**.
+	 * { var_description .
+	 */
+	private int draw;
+	/**.
+	 * Constructs the object.
+	 */
+	private Team() { }
+	/**.
+	 * Constructs the object.
+	 *
+	 * @param      inp   The inp.
+	 */
+	public Team(final String[] inp) {
 		teamname = inp[0];
 		wins = Integer.parseInt(inp[1]);
 		loses = Integer.parseInt(inp[2]);
 		draw = Integer.parseInt(inp[3]);
 	}
-	String geteam() {
+	/**.
+	 * { function_description .
+	 *
+	 * @return     { description_of_the_return_value }.
+	 */
+	public String geteam() {
 		return teamname;
 	}
-	void seteam(String tm) {
+	/**.
+	 * { function_description .
+	 *
+	 * @param      tm    The time.
+	 */
+	public void seteam(final String tm) {
 		teamname = tm;
 	}
-
-	int getwins() {
+	/**.
+	 * { function_description .
+	 *
+	 * @return     { description_of_the_return_value }.
+	 */
+	public int getwins() {
 		return wins;
 	}
-	void sewins(int win)  {
+	/**.
+	 * { function_description .
+	 *
+	 * @param      win   The window.
+	 */
+	public void sewins(final int win)  {
 		wins = win;
 	}
-
-	int getloses() {
+	/**.
+	 * { function_description .
+	 *
+	 * @return     { description_of_the_return_value }.
+	 */
+	public int getloses() {
 		return loses;
 	}
-	void seloses(int lose)  {
+	/**.
+	 * { function_description .
+	 *
+	 * @param      lose  The lose.
+	 */
+	public void seloses(final int lose)  {
 		loses = lose;
 	}
-
-	int getdraw() {
+	/**.
+	 * { function_description .
+	 *
+	 * @return     { description_of_the_return_value }.
+	 */
+	public int getdraw() {
 		return draw;
 	}
-	void sedraw(int dr)  {
+	/**.
+	 * { function_description .
+	 *
+	 * @param      dr    { parameter_description }.
+	 */
+	public void sedraw(final int dr)  {
 		draw = dr;
 	}
+	/**.
+	 * Returns a string representation of the object.
+	 *
+	 * @return     String representation of the object.
+	 */
 	public String toString() {
 		String str = "";
 		str = geteam();
