a002d27fe68e30e992ed46c0efb0c647a1d1a08c
gangadharsingh <gangadharsingh056@msitprogram.net>
2018-10-17 11:56:16 +0530

    "ads1_m8_a1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- assignment/m8/Assignment-1/8.1 Team Ranking With Insertion/Insertionsort.java
+++ assignment/m8/Assignment-1/8.1 Team Ranking With Insertion/Insertionsort.java
@@ -31,17 +31,17 @@ public class Insertionsort {
         for (int i = 1; i < size; i++) {
             for (int j = i; j > 0; j--) {
                 int max = j;
-                if (team[j-1].getwins() < team[j].getwins()) {
-                    max = j-1;
-                } else if (team[j].getwins() == team[j-1].getwins()) {
-                    if (team[j].getloses() < team[j-1].getloses()) {
-                        max = j-1;
+                if (team[j - 1].getwins() < team[j].getwins()) {
+                    max = j - 1;
+                } else if (team[j].getwins() == team[j - 1].getwins()) {
+                    if (team[j].getloses() < team[j - 1].getloses()) {
+                        max = j - 1;
                     }
-                    if (team[j].getloses() == team[j-1].getloses()) {
-                        if (team[j].getdraw() > team[j-1].getdraw()) {
-                            max = j-1;
-                            }
+                    if (team[j].getloses() == team[j - 1].getloses()) {
+                        if (team[j].getdraw() > team[j - 1].getdraw()) {
+                            max = j - 1;
                         }
+                    }
                 }
                 swap(team, j, max);
             }
--- assignment/m8/Assignment-1/8.1 Team Ranking With Insertion/Team.java
+++ assignment/m8/Assignment-1/8.1 Team Ranking With Insertion/Team.java
@@ -2,111 +2,127 @@
  * Class for team.
  */
 public class Team {
-	/**
-	 * teamname field.
-	 */
-	private String teamname;
-	/**
-	 * no of wins.
-	 */
-	private int wins;
-	/**
-	 * no of losses.
-	 */
-	private int loses;
-	/**
-	 * no of draw.
-	 */
-	private int draw;
-	/**
-	 * Constructs the object.
-	 * Best: O(1).
+    /**
+     * teamname field.
+     */
+    private String teamname;
+    /**
+     * no of wins.
+     */
+    private int wins;
+    /**
+     * no of losses.
+     */
+    private int loses;
+    /**
+     * no of draw.
+     */
+    private int draw;
+    /**
+     * Constructs the object.
+     * Best: O(1).
      * Avg: O(1).
      * Worst : O(1).
-	 * @param      inp   The inp.
-	 */
-	public Team(final String[] inp) {
-		teamname = inp[0];
-		wins = Integer.parseInt(inp[1]);
-		loses = Integer.parseInt(inp[2]);
-		draw = Integer.parseInt(inp[2+1]);
-	}
-	/**
-	 * get team name.
-	 *
-	 * @return     { respestive value of filds }.
-	 */
-	public String geteam() {
-		return teamname;
-	}
-	/**
-	 * { getters and setters method }.
-	 *
-	 * @param      tm    The time.
-	 */
-	public void seteam(final String tm) {
-		teamname = tm;
-	}
-	/**
-	 * { getters and setters method }.
-	 *
-	 * @return     { respestive value of filds }.
-	 */
-	public int getwins() {
-		return wins;
-	}
-	/**
-	 * { getters and setters method }.
-	 *
-	 * @param      win   The window.
-	 */
-	public void sewins(final int win)  {
-		wins = win;
-	}
-
-	/**
-	 * { getters and setters method }.
-	 *
-	 * @return     { respestive value of filds }.
-	 */
-	public int getloses() {
-		return loses;
-	}
-	/**
-	 * { getters and setters method }.
-	 *
-	 * @param      lose  The lose.
-	 */
-	public void seloses(final int lose)  {
-		loses = lose;
-	}
-
-	/**
-	 * { getters and setters method }.
-	 * 
-	 * @return     { respestive value of filds }.
-	 */
-	public int getdraw() {
-		return draw;
-	}
-	/**
-	 * { getters and setters method }.
-	 *
-	 * @param      dr    { parameter_description }.
-	 */
-	public void sedraw(final int dr)  {
-		draw = dr;
-	}
-	/**
-	 * Returns a string representation of the object.
-	 * Best: O(1).
+     * @param      inp   The inp.
+     */
+    public Team(final String[] inp) {
+        teamname = inp[0];
+        wins = Integer.parseInt(inp[1]);
+        loses = Integer.parseInt(inp[2]);
+        draw = Integer.parseInt(inp[2 + 1]);
+    }
+    /**
+     * get team name.
+     * Best: O(1).
      * Avg: O(1).
      * Worst : O(1).
-	 * @return     String representation of the object.
-	 */
-	public String toString() {
-		String str = "";
-		str = geteam();
-		return str;
-	}
+     * @return     { respestive value of filds }.
+     */
+    public String geteam() {
+        return teamname;
+    }
+    /**
+     * { getters and setters method }.
+     * Best: O(1).
+     * Avg: O(1).
+     * Worst : O(1).
+     * @param      tm    The time.
+     */
+    public void seteam(final String tm) {
+        teamname = tm;
+    }
+    /**
+     * { getters and setters method }.
+     * Best: O(1).
+     * Avg: O(1).
+     * Worst : O(1).
+     * @return     { respestive value of filds }.
+     */
+    public int getwins() {
+        return wins;
+    }
+    /**
+     * { getters and setters method }.
+     * Best: O(1).
+     * Avg: O(1).
+     * Worst : O(1).
+     * @param      win   The window.
+     */
+    public void sewins(final int win)  {
+        wins = win;
+    }
+
+    /**
+     * { getters and setters method }.
+     * Best: O(1).
+     * Avg: O(1).
+     * Worst : O(1).
+     * @return     { respestive value of filds }.
+     */
+    public int getloses() {
+        return loses;
+    }
+    /**
+     * { getters and setters method }.
+     * Best: O(1).
+     * Avg: O(1).
+     * Worst : O(1).
+     * @param      lose  The lose.
+     */
+    public void seloses(final int lose)  {
+        loses = lose;
+    }
+
+    /**
+     * { getters and setters method }.
+     * Best: O(1).
+     * Avg: O(1).
+     * Worst : O(1).
+     * @return     { respestive value of filds }.
+     */
+    public int getdraw() {
+        return draw;
+    }
+    /**
+     * { getters and setters method }.
+     * Best: O(1).
+     * Avg: O(1).
+     * Worst : O(1).
+     * @param      dr    { parameter_description }.
+     */
+    public void sedraw(final int dr)  {
+        draw = dr;
+    }
+    /**
+     * Returns a string representation of the object.
+     * Best: O(1).
+     * Avg: O(1).
+     * Worst : O(1).
+     * @return     String representation of the object.
+     */
+    public String toString() {
+        String str = "";
+        str = geteam();
+        return str;
+    }
 }
