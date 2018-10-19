350cf96ec83ed3d10a704c3320dff66333d47b5f
gangadharsingh <gangadharsingh056@msitprogram.net>
2018-10-19 15:45:43 +0530

    "ads1_week3_exam: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ assignment/m18/Week-3_Assignment-1/BinarySearchST.java
@@ -0,0 +1,196 @@
+import java.util.NoSuchElementException;
+class BinarySearchST<Key extends Comparable<Key>, Value> {
+    private static final int INIT_CAPACITY = 2;
+    private Key[] keys;
+    private Value[] vals;
+    private int n = 0;
+
+    /**
+     * Initializes an empty symbol table.
+     */
+    public BinarySearchST() {
+        this(INIT_CAPACITY);
+    }
+
+    /**
+     * Initializes an empty symbol table with the specified initial capacity.
+     * @param capacity the maximum capacity
+     */
+    public BinarySearchST(int capacity) { 
+        keys = (Key[]) new Comparable[capacity]; 
+        vals = (Value[]) new Object[capacity]; 
+    }   
+
+    // resize the underlying arrays
+    private void resize(int capacity) {
+        assert capacity >= n;
+        Key[]   tempk = (Key[])   new Comparable[capacity];
+        Value[] tempv = (Value[]) new Object[capacity];
+        for (int i = 0; i < n; i++) {
+            tempk[i] = keys[i];
+            tempv[i] = vals[i];
+        }
+        vals = tempv;
+        keys = tempk;
+    }
+
+    /**
+     * Returns the number of key-value pairs in this symbol table.
+     *
+     * @return the number of key-value pairs in this symbol table
+     */
+    public int size() {
+        return n;
+    }
+
+    /**
+     * Returns true if this symbol table is empty.
+     *
+     * @return {@code true} if this symbol table is empty;
+     *         {@code false} otherwise
+     */
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+
+    /**
+     * Does this symbol table contain the given key?
+     *
+     * @param  key the key
+     * @return {@code true} if this symbol table contains {@code key} and
+     *         {@code false} otherwise
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public boolean contains(Key key) {
+        if (key == null) throw new IllegalArgumentException("argument to contains() is null");
+        return get(key) != null;
+    }
+
+    /**
+     * Returns the value associated with the given key in this symbol table.
+     *
+     * @param  key the key
+     * @return the value associated with the given key if the key is in the symbol table
+     *         and {@code null} if the key is not in the symbol table
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public Value get(Key key) {
+        if (key == null) throw new IllegalArgumentException("argument to get() is null"); 
+        if (isEmpty()) return null;
+        int i = rank(key); 
+        if (i < n && keys[i].compareTo(key) == 0) return vals[i];
+        return null;
+    }
+    /**
+     * Returns the number of keys in this symbol table strictly less than {@code key}.
+     *
+     * @param  key the key
+     * @return the number of keys in the symbol table strictly less than {@code key}
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public int rank(Key key) {
+        if (key == null) throw new IllegalArgumentException("argument to rank() is null"); 
+
+        int lo = 0, hi = n-1; 
+        while (lo <= hi) { 
+            int mid = lo + (hi - lo) / 2; 
+            int cmp = key.compareTo(keys[mid]);
+            if      (cmp < 0) hi = mid - 1; 
+            else if (cmp > 0) lo = mid + 1; 
+            else return mid; 
+        } 
+        return lo;
+    } 
+
+
+
+    /**
+     * Inserts the specified key-value pair into the symbol table, overwriting the old 
+     * value with the new value if the symbol table already contains the specified key.
+     * Deletes the specified key (and its associated value) from this symbol table
+     * if the specified value is {@code null}.
+     *
+     * @param  key the key
+     * @param  val the value
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public void put(Key key, Value val)  {
+        if (key == null) throw new IllegalArgumentException("first argument to put() is null");
+        int i = rank(key);
+
+        // key is already in table
+        if (i < n && keys[i].compareTo(key) == 0) {
+            vals[i] = val;
+            return;
+        }
+
+        // insert new key-value pair
+        if (n == keys.length) resize(2*keys.length);
+
+        for (int j = n; j > i; j--)  {
+            keys[j] = keys[j-1];
+            vals[j] = vals[j-1];
+        }
+        keys[i] = key;
+        vals[i] = val;
+        n++;
+
+        assert check();
+    } 
+    /**
+     * Return the kth smallest key in this symbol table.
+     *
+     * @param  k the order statistic
+     * @return the {@code k}th smallest key in this symbol table
+     * @throws IllegalArgumentException unless {@code k} is between 0 and
+     *        <em>n</em>–1
+     */
+    public Key select(int k) {
+        if (k < 0 || k >= size()) {
+            throw new IllegalArgumentException("called select() with invalid argument: " + k);
+        }
+        return keys[k];
+    }
+    /**
+     * Returns the number of keys in this symbol table in the specified range.
+     *
+     * @param lo minimum endpoint
+     * @param hi maximum endpoint
+     * @return the number of keys in this symbol table between {@code lo} 
+     *         (inclusive) and {@code hi} (inclusive)
+     * @throws IllegalArgumentException if either {@code lo} or {@code hi}
+     *         is {@code null}
+     */
+    public int size(Key lo, Key hi) {
+        if (lo == null) throw new IllegalArgumentException("first argument to size() is null"); 
+        if (hi == null) throw new IllegalArgumentException("second argument to size() is null"); 
+
+        if (lo.compareTo(hi) > 0) return 0;
+        if (contains(hi)) return rank(hi) - rank(lo) + 1;
+        else              return rank(hi) - rank(lo);
+    }
+   /***************************************************************************
+    *  Check internal invariants.
+    ***************************************************************************/
+
+    private boolean check() {
+        return isSorted() && rankCheck();
+    }
+
+    // are the items in the array in ascending order?
+    private boolean isSorted() {
+        for (int i = 1; i < size(); i++)
+            if (keys[i].compareTo(keys[i-1]) < 0) return false;
+        return true;
+    }
+
+    // check that rank(select(i)) = i
+    private boolean rankCheck() {
+        for (int i = 0; i < size(); i++)
+            if (i != rank(select(i))) return false;
+        for (int i = 0; i < size(); i++)
+            if (keys[i].compareTo(select(rank(keys[i]))) != 0) return false;
+        return true;
+    }
+}
---
+++ assignment/m18/Week-3_Assignment-1/Solution.java
@@ -0,0 +1,128 @@
+import java.util.Scanner;
+// import java.util.Comparable;
+/**
+ * Class for solution.
+ */
+public class Solution {
+    /**
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner s = new Scanner(System.in);
+        int st = Integer.parseInt(s.nextLine());
+        BinarySearchST<String, Integer> beststock = new BinarySearchST<String, Integer> (st);
+        BinarySearchST<String, Integer> worststock = new BinarySearchST<String, Integer> (st);
+        for (int i = 0; i < 6; i++) {
+            MaxPQ<Stock> max = new MaxPQ<Stock> (st);
+            MinPQ<Stock> min = new MinPQ<Stock> (st);
+            for (int j = 0; j < st; j++) {
+                String[] str = s.nextLine().split(",");
+                max.insert(new Stock(str));
+                min.insert(new Stock(str));
+            }
+            for(int j = 0; j < 5; j++) {
+                Stock empty = max.delMax();
+                System.out.println(empty);
+                beststock = putInBST(empty, beststock);
+            }
+            System.out.println();
+            for (int j = 0; j < 5; j++) {
+                Stock empty = min.delMin();
+                System.out.println(empty);
+                worststock = putInBST(empty, worststock);
+            }
+            System.out.println();
+        }
+        int query = Integer.parseInt(s.nextLine());
+        for (int i = 0; i < query; i++) {
+            String[] str = s.nextLine().split(",");
+            switch(str[0]) {
+                case "get":
+                    if (str[1].equals("maxST")) {
+                        if (beststock.contains(str[2])) {
+                            System.out.println(beststock.get(str[2]));
+                        } else {
+                            System.out.println(0);
+                        }
+                    } else if (str[1].equals("minST")) {
+                        if (worststock.contains(str[2])) {
+                            System.out.println(worststock.get(str[2]));
+                        } else {
+                            System.out.println(0);
+                        }
+                    }
+                    break;
+                case "intersection":
+                    for (i = 0; i < beststock.size(); i++) {
+                        for (int j = 0; j < worststock.size(); j++) {
+                            if ((beststock.select(i)).equals(worststock.select(j))) {
+                                System.out.println(beststock.select(i));
+                            }
+                        }
+                    }
+                    break;
+            }
+        }
+        
+    }
+    public static BinarySearchST<String, Integer> putInBST(final Stock key, final BinarySearchST<String, Integer> bst) {
+        if (bst.contains(key.getname())) {
+            bst.put(key.getname(), bst.get(key.getname()) + 1);
+        }
+        else {
+            bst.put(key.getname(), 1);
+        }
+        return bst;
+    }
+}
+/**
+ * Class for stock.
+ */
+class Stock implements Comparable<Stock> {
+    public String nm;
+    public float change;
+    Stock(String[] a) {
+        nm = a[0];
+        change = Float.parseFloat(a[1]);
+    }
+    /**
+     * { function_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public String getname() {
+        return nm;
+    }
+    /**
+     * { function_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public Float getchange() {
+        return change;
+    }
+    /**
+     * { function_description }
+     *
+     * @param      that  The that
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int compareTo(final Stock that) {
+        if (this.getchange() > that.getchange()) return 1;
+        else if (this.getchange() < that.getchange()) return -1;
+        else if ((this.getname().compareTo(that.getname())) > 0) return 1;
+        else if ((this.getname().compareTo(that.getname())) < 0) return -1;
+        else return 0;
+    }
+    /**
+     * Returns a string representation of the object.
+     *
+     * @return     String representation of the object.
+     */
+    public String toString() {
+        return getname() + " " + getchange();
+    }
+}
