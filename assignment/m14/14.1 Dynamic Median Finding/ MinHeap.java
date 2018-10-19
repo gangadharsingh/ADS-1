eb0fc66b9215eb16dd1a31fa33c62f90fbbdf87d
gangadharsingh <gangadharsingh056@msitprogram.net>
2018-10-19 09:26:16 +0530

    "ads1_m14_a1: 1/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignment/m14/14.1 Dynamic Median Finding/ MinHeap.java
+++ assignment/m14/14.1 Dynamic Median Finding/ MinHeap.java
@@ -1,23 +1,23 @@
 import java.util.Comparator;
-/**
+/**.
  * Class for minimum heap.
  *
  * @param      <Key>  The key.
  */
-class MinHeap<Key> {
-	/**
+final class MinHeap<Key> {
+	/**.
 	 * generic array.
 	 */
-	public Key[] hp;
-	/**
+	private Key[] hp;
+	/**.
 	 * size of array.
 	 */
-	public int size;
-	/**
+	private int size;
+	/**.
 	 * Comparator of generic type.
 	 */
-	public Comparator<Key> comparator;
-	/**
+	private Comparator<Key> comparator;
+	/**.
 	 * Constructs the object.
 	 *
 	 * @param      sz    The size.
@@ -26,13 +26,13 @@ class MinHeap<Key> {
 		hp = (Key[]) new Object[sz + 1];
 		size = 0;
 	}
-	/**
+	/**.
 	 * Constructs the object.
 	 */
 	public MinHeap() {
 		this(1);
 	}
-	/**
+	/**.
 	 * getting size of array.
 	 *
 	 * @return     integer.
@@ -40,9 +40,11 @@ class MinHeap<Key> {
 	public int getsize() {
 		return size;
 	}
-	/**
+	/**.
 	 * inserting Key type element.
-	 *
+	 *  Best case: O(1)
+     *  worst case :O(1)
+     *  Average case: O(1)
 	 * @param      k     generic type.
 	 */
 	public void insert(final Key k) {
@@ -52,35 +54,42 @@ class MinHeap<Key> {
 		hp[++size] = k;
 		swim(size);
 	}
-	/**
+	/**.
 	 * swimmin up the elements.
-	 *
+	 *  Best case: O(logN)
+     *  worst case :O(logN)
+     *  Average case: O(logN)
 	 * @param      ind   The ind
 	 */
-	public void swim(int ind) {
-		while (ind > 1 && greater(ind / 2, ind)) { //ind is child and ind/2 is parent.
-			swap(ind, ind / 2); //exchange parent with child when parent is less than child.
-			ind = ind / 2;
+	public void swim(final int ind) {
+        int index = ind;
+		while (index > 1 && greater(index / 2, index)) { //index is child and index/2 is parent.
+			swap(index, index / 2); //exchange parent with child when parent is less than child.
+			index = index / 2;
 		}
 	}
-	/**
+	/**.
 	 * comparing two elements.
-	 *
+	 *  Best case: O(1)
+     *  worst case :O(1)
+     *  Average case: O(1)
 	 * @param      i     integer value
 	 * @param      j     integer value
 	 *
 	 * @return     boolean.
 	 */
-	public boolean greater(int i, int j) {
+	public boolean greater(final int i, final int j) {
 		if (comparator == null) {
 			return  ((Comparable<Key>) hp[i]).compareTo(hp[j]) > 0;
 		} else {
 			return comparator.compare(hp[i], hp[j]) > 0;
 		}
 	}
-	/**
+	/**.
 	 * deleting minimum.
-	 *
+	 *  Best case: O(1)
+     *  worst case :O(1)
+     *  Average case: O(1)
 	 * @return     Key type.
 	 */
 	Key delMin() {
@@ -93,54 +102,66 @@ class MinHeap<Key> {
 		}
 		return min;
 	}
-	/**
+	/**.
 	 * Gets the minimum.
-	 *
+	 *  Best case: O(1)
+     *  worst case :O(1)
+     *  Average case: O(1)
 	 * @return     The minimum.
 	 */
 	Key getMin() {
 		return hp[1];
 	}
-	/**
+	/**.
 	 * { function_description }
-	 *
+	 *  Best case: O(logN)
+     *  worst case :O(logN)
+     *  Average case: O(logN)
 	 * @param      a     { parameter_description }
 	 */
-	void sink(int a) {
-		while (2 * a <= size) {
+	void sink(final int a) {
+        int b = a;
+		while (2 * b <= size) {
 			int j = 2 * a;
 			if (j < size && greater(j, j + 1)) {
 				j++;
 			}
-			swap(a, j);
-			a = j;
+			swap(b, j);
+			b = j;
 		}
 	}
-	/**
+	/**.
 	 * { function_description }
-	 *
+	 *  Best case: O(N)
+     *  worst case :O(N)
+     *  Average case: O(N)
 	 * @param      newsize  The newsize
 	 */
-	void resize(int newsize) {
+	void resize(final int newsize) {
 		Key[] temp = (Key[]) new Object[newsize];
 		for (int i = 1; i <= size; i++) {
 			temp[i] = hp[i];
 		}
 		hp = temp;
 	}
-	/**
+	/**.
 	 * { function_description }
-	 *
+	 *  Best case: O(1)
+     *  worst case :O(1)
+     *  Average case: O(1)
 	 * @param      i     { parameter_description }
 	 * @param      j     { parameter_description }
 	 */
-	void swap(int i, int j) {
+	void swap(final int i, final int j) {
 		Key temp = hp[i];
 		hp[i] = hp[j];
 		hp[j] = temp;
 	}
-	/**
+	/**.
 	 * { function_description }
+     *  Best case: O(1)
+     *  worst case :O(1)
+     *  Average case: O(1)
 	 */
 	void show() {
 		for (int i = 1; i < size + 1; i++) {
