9a585f6ae3ccdca6d9521a4cb1455f1052fc489c
gangadharsingh <gangadharsingh056@msitprogram.net>
2018-10-28 18:50:02 +0530

    "ads1_m22_a1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignment/m22/22.1 Ransom Note/Solution.java
+++ assignment/m22/22.1 Ransom Note/Solution.java
@@ -1,52 +1,134 @@
 import java.util.Scanner;
 import java.util.Arrays;
-class HashTable<Key, Value> {
-    int keyCount;
-    int size;
-    Node[] node = (Node[]) new Object[size];
+/**
+ * Class for hash table.
+ */
+class HashTable {
+    /**
+     * Class for node.
+     */
     class Node {
-        Key key;
-        Value value;
-        Node next;
-        Node(Key k, Value v) {
-            key = k;
-            value = v;
-            next = null;
+        /**
+         * word as key.
+         */
+        private String key;
+        /**
+         * count as value.
+         */
+        private Integer val;
+        /**
+         * next node.
+         */
+        private Node next;
+        /**
+         * Constructs the object.
+         *
+         * @param      k     { parameter_description }
+         * @param      v     { parameter_description }
+         * @param      n     { parameter_description }
+         */
+        Node(final String k, final Integer v, final Node n) {
+            this.key = k;
+            this.val = v;
+            this.next = n;
         }
-        public Key getkey() {
-            return key;
+        /**
+         * retunrs key.
+         *
+         * @return     { description_of_the_return_value }
+         */
+        String getkey() {
+            return this.key;
         }
-        public Value getvalue() {
-            return value;
+        /**
+         * Gets the value.
+         *
+         * @return     The value.
+         */
+        Integer getValue() {
+            return this.val;
         }
-        public void setvalue(Value v) {
-            value = v;
+        /**
+         * sets value.
+         *
+         * @param      v     { parameter_description }
+         */
+        void setvalue(final Integer v) {
+            this.val = v;
         }
     }
-    private int hash(Key k) {
-        return (k.hashCode() & 0x7fffffff) % size;
+    /**
+     * nodes table.
+     */
+    private Node[] st;
+    /**
+     * size.
+     */
+    private int s = (2 * (2 + 2 + 1)) * (2 * (2 + 2 + 1));
+    /**
+     * Constructs the object.
+     */
+    HashTable() {
+        st = new Node[s];
     }
-    public void put(Key k, Value v) {
+    /**
+     * hash.
+     *
+     * @param      k     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    int hash(final String k) {
+        final Integer num = 0x7fffffff;
+        return (k.hashCode() & num) % s;
+    }
+    /**
+     * resizes.
+     */
+    public void resize() {
+        st = Arrays.copyOf(st, 2 * s);
+    }
+    /**
+     * put.
+     * Best case: O(k)
+     * Average case: O(K)
+     * Worst case: O(logN)
+     * @param      k     { parameter_description }
+     * @param      v     { parameter_description }
+     */
+    public void put(final String k, final Integer v) {
         int i = hash(k);
-        for (Node nod = node[i]; nod != null; nod = nod.next) {
-            if (k.equals(nod.key)) {
-                nod.value = v;
-            } else {
-                node[i] = new Node(k, v);
+        for (Node x = st[i]; x != null; x = x.next) {
+            if (k.equals(x.getkey())) {
+                x.setvalue(x.getValue() + 1);
+                return;
             }
         }
+        if (i >= st.length) {
+            resize();
+        }
+        st[i] = new Node(k, v, st[i]);
     }
-    public boolean get(Key key) {
-        int i = hash(key);
-        for (Node x = node[i]; x != null; x = x.next)
-            if (key.equals(x.key)) {
-                if(x.getvalue().compareTo(0) < 0) {
-                    x.setvalue(x.setvalue(value));
+    /**
+     * get.
+     * Best case: O(k)
+     * Average case: O(K)
+     * Worst case: O(logN)
+     * @param      k     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public boolean get(final String k) {
+        int i = hash(k);
+        for (Node x = st[i]; x != null; x = x.next) {
+            if (k.equals(x.getkey())) {
+                if (x.getValue() > 0) {
+                    x.setvalue(x.getValue() - 1);
                     return true;
-                }else {
-                    return false;
                 }
+                return false;
             }
+        }
         return false;
     }
 }
