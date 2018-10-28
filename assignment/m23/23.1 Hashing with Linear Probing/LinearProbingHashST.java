10f55b99ed69810eb1e84eea2aa6f66ea7cb8c92
gangadharsingh <gangadharsingh056@msitprogram.net>
2018-10-28 17:27:21 +0530

    "ads1_m23_a1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignment/m23/23.1 Hashing with Linear Probing/LinearProbingHashST.java
+++ assignment/m23/23.1 Hashing with Linear Probing/LinearProbingHashST.java
@@ -36,7 +36,7 @@ public class LinearProbingHashST<Key, Value> {
      *
      * @param capacity the initial capacity
      */
-    public LinearProbingHashST(int capacity) {
+    public LinearProbingHashST(final int capacity) {
         m = capacity;
         n = 0;
         keys = (Key[])   new Object[m];
@@ -68,19 +68,30 @@ public class LinearProbingHashST<Key, Value> {
      * @throws IllegalArgumentException if {@code key} is {@code null}
      */
     public boolean contains(final Key key) {
-        if (key == null) throw new IllegalArgumentException("argument to contains() is null");
+        if (key == null) throw new IllegalArgumentException(
+            "argument to contains() is null");
         return get(key) != null;
     }
-    // hash function for keys - returns value between 0 and M-1
-    private int hash(Key key) {
+    /**.
+     * { function_description }
+     *
+     * @param      key   The key
+     *
+     * @return     { description_of_the_return_value }
+     */
+    private int hash(final Key key) {
         // return (key.hashCode() & 0x7fffffff) % m;
         String s = (String)key;
         return ((int)s.charAt(0) * (2 * 2 * 2 + 2 + 1)) % m;
     }
-    // resizes the hash table to the given capacity by re-hashing all of the keys
-    //time complexity - O(n).
-    private void resize(int capacity) {
-        LinearProbingHashST<Key, Value> temp = new LinearProbingHashST<Key, Value>(capacity);
+    /**.
+     * { function_description }
+     *time complexity - O(n).
+     * @param      capacity  The capacity
+     */
+    private void resize(final int capacity) {
+        LinearProbingHashST<Key, Value> temp = new 
+        LinearProbingHashST<Key, Value>(capacity);
         for (int i = 0; i < m; i++) {
             if (keys[i] != null) {
                 temp.put(keys[i], vals[i]);
@@ -90,7 +101,6 @@ public class LinearProbingHashST<Key, Value> {
         vals = temp.vals;
         m    = temp.m;
     }
-
     /**.
      * Inserts the specified key-value pair into the symbol table, overwriting the old
      * value with the new value if the symbol table already contains the specified key.
@@ -101,8 +111,10 @@ public class LinearProbingHashST<Key, Value> {
      * @param  val the value
      * @throws IllegalArgumentException if {@code key} is {@code null}
      */
-    public void put(Key key, Value val) {
-        if (key == null) throw new IllegalArgumentException("first argument to put() is null");
+    public void put(final Key key, final Value val) {
+        if (key == null) {
+            throw new IllegalArgumentException("first argument to put() is null");
+        }
 
         if (val == null) {
             delete(key);
@@ -132,11 +144,15 @@ public class LinearProbingHashST<Key, Value> {
      *         {@code null} if no such value
      * @throws IllegalArgumentException if {@code key} is {@code null}
      */
-    public Value get(Key key) {
-        if (key == null) throw new IllegalArgumentException("argument to get() is null");
-        for (int i = hash(key); keys[i] != null; i = (i + 1) % m)
-            if (keys[i].equals(key))
+    public Value get(final Key key) {
+        if (key == null) {
+            throw new IllegalArgumentException("argument to get() is null");
+        }
+        for (int i = hash(key); keys[i] != null; i = (i + 1) % m) {
+            if (keys[i].equals(key)) {
                 return vals[i];
+            }
+        }
         return null;
     }
 
@@ -148,8 +164,12 @@ public class LinearProbingHashST<Key, Value> {
      * @throws IllegalArgumentException if {@code key} is {@code null}
      */
     public void delete(Key key) {
-        if (key == null) throw new IllegalArgumentException("argument to delete() is null");
-        if (!contains(key)) return;
+        if (key == null) {
+            throw new IllegalArgumentException("argument to delete() is null");
+        }
+        if (!contains(key)) {
+            return;
+        }
 
         // find position i of key
         int i = hash(key);
@@ -177,7 +197,9 @@ public class LinearProbingHashST<Key, Value> {
         n--;
 
         // halves size of array if it's 12.5% full or less
-        if (n > 0 && n <= m / (2 * 2 * 2)) resize(m / 2);
+        if (n > 0 && n <= m / (2 * 2 * 2)) {
+            resize(m / 2);
+        }
 
         assert check();
     }
@@ -191,13 +213,18 @@ public class LinearProbingHashST<Key, Value> {
      */
     public Iterable<Key> keys() {
         Queue<Key> queue = new Queue<Key>();
-        for (int i = 0; i < m; i++)
-            if (keys[i] != null) queue.enqueue(keys[i]);
+        for (int i = 0; i < m; i++) {
+            if (keys[i] != null) {
+                queue.enqueue(keys[i]);
+            }
+        }
         return queue;
     }
-
-    // integrity check - don't check after each put() because
-    // integrity not maintained during a delete()
+    /**.
+     * { function_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
     private boolean check() {
 
         // check that hash table is at most 50% full
@@ -208,8 +235,9 @@ public class LinearProbingHashST<Key, Value> {
 
         // check that each key in table can be found by get()
         for (int i = 0; i < m; i++) {
-            if (keys[i] == null) continue;
-            else if (get(keys[i]) != vals[i]) {
+            if (keys[i] == null) {
+                continue;
+            } else if (get(keys[i]) != vals[i]) {
                 System.err.println("get[" + keys[i] + "] = " + get(keys[i]) + "; vals[i] = " + vals[i]);
                 return false;
             }
